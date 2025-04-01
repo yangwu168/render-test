f8884a26fce9e236f2561ed09ee4efb28da40890
Yang Wu <yang@Yangs-Laptop.local>
2025-04-01 01:01:59 -0500

    remove cors, update frontend vite config, add scripts

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
 
 app.use(express.static('dist'));
-app.use(cors());
 app.use(express.json());
 
 let notes = [
